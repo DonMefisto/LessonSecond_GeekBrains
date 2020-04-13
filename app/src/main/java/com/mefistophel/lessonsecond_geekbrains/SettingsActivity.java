b166727477cd760be9961e8cb524637c69f3c5a9
Andrey <ra@799000.ru>
2020-04-13 14:41:13 +1000

    I start to code the application

---
+++ app/src/main/java/com/mefistophel/lessonsecond_geekbrains/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.mefistophel.lessonsecond_geekbrains;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_settings);
+    }
+}
